d4a0900ba273350521a9aef6113ec5c0545edd47
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2025-03-09 20:25:20 +0200

    created signup html

---
+++ article-client/scripts/login.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
+
+  loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    // Get input values
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      // Send request with Axios
+      const response = await axios.post(
+        "http://localhost/FAQ%20PROJECT/article-server/apis/v1/login.php",
+        { email, password },
+        { headers: { "Content-Type": "application/json" } }
+      );
+
+      if (response.data.status === "success") {
+        localStorage.setItem("token", response.data.data);
+        window.location.href = "./";
+      } else {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      alert(
+        "Error: " + (error.response?.data?.message || "Something went wrong")
+      );
+    }
+  });
+});
